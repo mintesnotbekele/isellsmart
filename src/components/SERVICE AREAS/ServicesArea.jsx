cbbb1e3db97f9c3449c360c28ba48a2737dfdea6
DAdawit <58243647+DAdawit@users.noreply.github.com>
2023-08-31 15:19:54 +0300

    alignment fix

--- src/components/SERVICE AREAS/ServicesArea.jsx
+++ src/components/SERVICE AREAS/ServicesArea.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import styles from "./services.module.css";
-import { services } from "./data";
 const ServicesArea = () => {
   return (
     <>
       <h3 className={styles.header}>SERVICE AREAS</h3>
       <section className={styles.container}>
         <div className={styles.servicesContainer}>
-          {/* {services.map((service, index) => (
-            <div key={index} className={styles.card}>
-              <h1 className={styles.title}>{service}</h1>
-            </div>
-          ))} */}
           <div className={styles.card}>
             <h1 className={styles.title}>carlsbad </h1>
             <h1 className={styles.title}>Encinitas</h1>
